1e1643636f6e1434da739f694bcb5246ac771bfa
Пользователь <@DESKTOP-6V93RUO>
2022-09-16 11:08:00 +0300

    Добавьте файлы проекта.

---
+++ hw5_2/Program.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace hw5_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                IFunctions p1 = new Passenger { Stamp = "Volvo", Model = "XC90", Cost = 7000000, Type = "Кроссовер", PassengerNumbers = 5 };
+                IFunctions c1 = new Cargo { Stamp = "Hyundai", Model= "HD78", Cost = 2900000, Type = "Рефрижиратор", Capacity = 5 };
+                IFunctions m1 = new Moto {Stamp = "Kawasaki", Model = "Z 1000SX", Cost = 1400000, Type = "Спорт-туризм", Power = 142 };
+
+                IFunctions pl1 = new Plane { Stamp ="Zodiac", Model="CH 650", Cost =3300000, NumberEngines = 1, Distance = 925 };
+                IFunctions h1 = new Helicopter {Stamp= "McDonnel-Douglas", Model="MD 900 Explorer", Cost = 75000000, NumberEngines = 2, NumberScrews = 1 };
+                IFunctions a1 = new Airship { Stamp = "AY-30", Model = "AY-30", Cost = 2500000, NumberEngines = 2, FillingGas = "Гелий" };
+
+                Park park = new Park();
+                park.park = new List<IFunctions> {p1, c1, m1, pl1, h1, a1 };
+
+                while (true)
+                {
+                    WriteLine("Выберете действие, которое хотите произвести:\n " +
+                        "1. Распечатать весь автопарк; \n " +
+                        "2. Распечатать парк наземного транспорта; \n " +
+                        "3. Распечатать парк воздушного транспорта; \n " +
+                        "4. Стоимость всего автопарка; \n " +
+                        "5. Стоимость наземного транспорта; \n " +
+                        "6. Стоимость воздушного транспорта; \n " +
+                        "7. Выйти из программы.");
+                    WriteLine();
+
+                    int posnum = Convert.ToInt32(ReadLine());
+
+                    switch (posnum)
+                    {
+                        case 1:
+                            {
+                                foreach (IFunctions item in park.park)
+                                {
+                                    WriteLine(item);
+                                    WriteLine($"{item.IsWork()}  {item.Work()}  {item.Transfer()}");
+                                };
+                            }
+                            break;
+                        case 2:
+                            {
+                                WriteLine("Наземный транспорт: ");
+                                foreach (IFunctions item in park.park)
+                                {
+                                    if (item is Ground)
+                                    {
+                                        WriteLine($"{item.Work()}  {item}");
+                                    }
+                                };
+                            }
+                            break;
+                        case 3:
+                            {
+                                WriteLine("Воздушный транспорт: ");
+                                foreach (IFunctions item in park.park)
+                                {
+                                    if (item is Air)
+                                    {
+                                        WriteLine($"{item.Work()}  {item}");
+                                    }
+                                };
+                            }
+                            break;
+                        case 4:
+                            {
+                                WriteLine($"Стоимость всего наземного транспорта = {(park as IPark).Costs()}\n");
+                            }
+                            break;
+                        case 5:
+                            {
+                                Park parkA = new Park();
+                                parkA.park = new List<IFunctions> {};
+                                foreach (IFunctions item in park.park)
+                                {
+                                    if (item.IsWork() == "Наземный транспорт")
+                                    {
+                                        parkA.park.Add(item);
+                                    }
+                                };
+                                WriteLine($"Стоимость наземного транспорта = {(parkA as IPark).Costs()}\n");
+                            }
+                            break;
+                        case 6:
+                            {
+                                Park parkV = new Park();
+                                parkV.park = new List<IFunctions> { };
+
+                                foreach (IFunctions item in park.park)
+                                {
+                                    if (item is Air)
+                                    {
+                                        parkV.park.Add(item);
+                                    }
+                                };
+                                WriteLine($"Стоимость воздушного транспорта = {(parkV as IPark).Costs()}\n");
+                            }
+                            break;
+                        case 7:
+                            return;
+
+                        default:
+                            WriteLine("Вы выбрали несуществующий пункт меню. Выберите снова.");
+                            continue;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ hw5_2/TC.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace hw5_2
+{
+    abstract class TC
+    {
+        public string Stamp { get; set; }
+        public string Model { get; set; }
+        public int Cost { get; set; }
+        public override string ToString()
+        {
+            return $"\nМарка: {Stamp} Модель: {Model} Стоимость: {Cost} руб.";
+        }
+    }
+
+    abstract class Ground : TC
+    {
+        public string Type { get; set; }
+        public override string ToString()
+        {
+            return base.ToString()+$" Тип: {Type}";
+        }
+    }
+    abstract class Air : TC
+    {
+        public int NumberEngines { get; set; }
+        public override string ToString()
+        {
+            return base.ToString() + $" Вид: {NumberEngines}";
+        }
+    }
+
+    interface IFunctions
+    {
+        string IsWork();
+        string Work();
+        string Transfer(); //перевожу
+    }
+
+    interface IPark
+    {
+        List<IFunctions> park { get; set; }
+        int Costs();
+    }
+
+    class Passenger : Ground, IFunctions
+    {
+        public int PassengerNumbers { get; set; }
+        public string IsWork()
+        {
+            return "Наземный транспорт"; 
+        }
+
+        public string Transfer()
+        {
+            return "Перевожу пассажиров. ";
+        }
+        public string Work()
+        {
+            return "Легковой. ";
+        }
+        public override string ToString()
+        {
+            return base.ToString() + $" Количество пассажиров: {PassengerNumbers} чел.";
+        }
+
+    }
+
+    class Cargo : Ground, IFunctions
+    {
+        public int Capacity { get; set; }
+        public string IsWork()
+        { 
+            return "Наземный транспорт"; 
+        }
+        public string Transfer()
+        {
+            return "Перевожу грузы. ";
+        }
+        public string Work()
+        {
+            return "Грузовой. ";
+        }
+        public override string ToString()
+        {
+            return base.ToString() + $" Грузоподъемность: {Capacity} т";
+        }
+        
+    }
+
+    class Moto : Ground, IFunctions
+    {
+        public int Power { get; set; }
+
+        public string IsWork()
+        {
+            return "Наземный транспорт";
+        }
+
+        public string Transfer()
+        {
+            return "Катаю по красивым местам. ";
+        }
+        public string Work()
+        {
+            return "Мотоцикл. ";
+        }
+        public override string ToString()
+        {
+            return base.ToString() + $" Мощность: {Power} л.с.";
+        }      
+    }
+
+    class Plane : Air, IFunctions
+    {
+        public int Distance { get; set; }
+        public string IsWork()
+        {
+            return "Воздушный транспорт";
+        }
+        
+        public string Transfer()
+        {
+            return "Перевожу пассажиров на другие континенты. ";
+        }
+        public string Work()
+        {
+            return "Самолет. ";
+        }
+        public override string ToString()
+        {
+            return base.ToString() + $" Дальность: {Distance} км";
+        }
+        
+    }
+
+    class Helicopter : Air, IFunctions
+    {
+        public int NumberScrews { get; set; }
+        public string IsWork()
+        {
+            return "Воздушный транспорт";
+        }
+
+        public string Transfer()
+        {
+            return "Перевожу грузы на другие континенты. ";
+        }
+        public string Work()
+        {
+            return "Вертолет. ";
+        }
+        
+        public override string ToString()
+        {
+            return base.ToString() + $" Количество несущих винтов: {NumberScrews}";
+        }
+    }
+
+    class Airship : Air, IFunctions
+    {
+        public string FillingGas { get; set; }
+        public string IsWork()
+        {
+            return "Воздушный транспорт"; 
+        }
+
+        public string Transfer()
+        {
+            return "Несу рекламу на борту. ";
+        }
+        public string Work()
+        {
+            return "Дирижабль. ";
+        }
+        public override string ToString()
+        {
+            return base.ToString() + $" Заполняющий газ: {FillingGas}";
+        }   
+    }
+
+    class Park : TC, IPark
+    {
+        public List<IFunctions> park { get; set; }
+
+        int IPark.Costs()
+        {
+            int sum = 0;
+            foreach ( TC item in park)
+            {
+                sum += item.Cost;
+            }
+            return sum;
+        }
+    }
+}
